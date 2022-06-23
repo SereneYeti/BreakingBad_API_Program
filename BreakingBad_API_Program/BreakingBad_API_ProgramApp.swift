08759d7153487662959f49f23abd03f796ef1651
James D Vieyra <james@vieyra.co.za>
2022-06-23 16:43:55 +0200

    Initial Commit

---
+++ BreakingBad_API_Program/BreakingBad_API_ProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBad_API_ProgramApp.swift
+//  BreakingBad_API_Program
+//
+//  Created by IACD-011 on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBad_API_ProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakingBad_API_Program/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreakingBad_API_Program
+//
+//  Created by IACD-011 on 2022/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
